a9cc434e2507f537771c3a551ed5ac29007ce342
Илья <ilya6381@yandex.ru>
2026-02-01 13:24:13 +0300

    5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,30 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Race race = new Race();
+        for(int i = 0; i <= 2; i += 1){
+            System.out.println("— Введите название машины №" + (i + 1) + ":");
+            String name = scanner.next();
+
+            while (true){
+                System.out.println("— Введите скорость машины №" + (i + 1) + ":");
+                try{
+                    int speed = Integer.parseInt(scanner.next());
+                    System.out.println();
+                    if(speed > 0 && speed <= 250){
+                        race.controlLead(new Car(name, speed));
+                        break;
+                    }
+                    else{
+                        System.out.println("— Неправильная скорость");
+                    }
+                }
+                catch (NumberFormatException e){
+                    System.out.println("— Неправильная скорость");
+                }
+            }
+        }
+        System.out.println("Самая быстрая машина:" + race.lead);
     }
 }
